3dd9d8c99e35d53effa2bd5735f35e8492bad878
Ritesh Gaur <gaur.ritesh@gmail.com>
2020-05-01 13:34:00 +0800

    after years of exp. How can I just not call the update fun and wonder wtf happended?
    Stupid me

--- Paddle.lua
+++ Paddle.lua
@@ -3,18 +3,18 @@ Paddle = Class {}
 function Paddle:init(x, y, width, height)
     self.x = x
     self.y = y
-    self.width = width 
+    self.width = width
     self.height = height
     self.dy = 0
 end
 
 function Paddle:update(dt)
     if self.dy < 0 then
+        -- player1Y = math.max(0, player1Y - PADDEL_SPEED * dt)
         self.y = math.max(0, self.y + self.dy * dt)
     elseif self.dy > 0 then
         self.y = math.min(v_height - 20, self.y + self.dy * dt)
     end
-    -- body
 end
 
 function Paddle:render()
--- main.lua
+++ main.lua
@@ -10,16 +10,26 @@ WINDOWS_HEIGHT = 720
 v_width = 432
 v_height = 243
 
+
+
 function love.load()
+    gameState = "start"
+    PADDLE_SPEED = 200
+
+    -- randomize stuff (kind a)
     math.randomseed(os.time())
+
+    --load an image
     RGnR = love.graphics.newImage("RGnR.png")
-    
+
+    --calling the constractor
     paddle1 = Paddle(5, 20, 5, 20)
     paddle2 = Paddle(v_width - 10, v_height - 30, 5, 20)
 
     sound = love.audio.newSource("bck_sound.ogg", "stream")
 
     love.graphics.setDefaultFilter("nearest", "nearest")
+
     smallFont = love.graphics.newFont("04B_03__.TTF", 20) --for hello
     scoreFont = love.graphics.newFont("04B_03__.TTF", 40) --for score
 
@@ -29,17 +39,11 @@ function love.load()
     player1Y = 30
     player2Y = v_height - 40
 
-
-
     ball_x = v_width / 2 - 2
     ball_y = v_height / 2 - 2
 
     ballDX = math.random(2) == 1 and -100 or 100
     ballDY = math.random(-50, 50)
-
-    gameState = "start"
-
-    PADDLE_SPEED = 200
 
     push:setupScreen(
         v_width,
@@ -55,22 +59,26 @@ function love.load()
 end
 
 function love.update(dt)
+
+    paddle1:update(dt)
+    paddle2:update(dt)
+
     if love.keyboard.isDown("w") then
-        -- player1Y = math.max(0, player1Y - PADDEL_SPEED * dt)
         paddle1.dy = -PADDLE_SPEED
+        --player1Y = math.max(0, player1Y - PADDLE_SPEED * dt)
     elseif love.keyboard.isDown("s") then
-        -- player1Y = math.min(v_height - 20, player1Y + PADDEL_SPEED * dt)
         paddle1.dy = PADDLE_SPEED
+        --player1Y = math.min(v_height - 20, player1Y + PADDLE_SPEED * dt)
     else
         paddle1.dy = 0
     end
 
     if love.keyboard.isDown("up") then
-        -- player2Y = math.max(0, player2Y - PADDEL_SPEED * dt)
         paddle2.dy = -PADDLE_SPEED
+        -- player2Y = math.max(0, player2Y - PADDLE_SPEED * dt)
     elseif love.keyboard.isDown("down") then
-        -- player2Y = math.min(v_height - 20, player2Y + PADDEL_SPEED * dt)
         paddle2.dy = PADDLE_SPEED
+        -- player2Y = math.min(v_height - 20, player2Y + PADDLE_SPEED * dt)
     else
         paddle2.dy = 0
     end
@@ -85,8 +93,6 @@ function love.draw()
     push:apply("start")
 
     love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
-    paddle1:render()
-    paddle2:render()
 
     --ball
     love.graphics.rectangle("fill", ball_x, ball_y, 5, 5)
@@ -101,6 +107,14 @@ function love.draw()
         love.audio.play(sound)
     end
 
+ paddle1:render()
+ paddle2:render()
+    
+    --left bar
+    -- love.graphics.rectangle('fill', 5,player1Y,5,20)
+    -- right bar
+    -- love.graphics.rectangle('fill', v_width -10,player2Y,5,20) 
+
     love.graphics.setFont(scoreFont)
     love.graphics.print(player1, v_width / 2 - 50, v_height / 5)
     love.graphics.print(player2, v_width / 2 + 50, v_height / 5)
@@ -110,7 +124,6 @@ end
 
 function love.keypressed(key)
     if key == "escape" then
-        --love.sound.beep()
         love.event.quit()
     elseif key == "enter" or key == "return" then
         if gameState == "start" then
